46d6e1fe7dbf1ff59e117299d78577d90ce192cb
SoftIce <32594805+SoftIcePink@users.noreply.github.com>
2020-02-19 23:03:35 +0100

    Models changed. DB connexion added.

---
+++ models/dbconnexion.php
@@ -0,0 +1,26 @@
+<?php
+
+//  Projet      :   Portefolio - M152
+//  Author      :   SoftIcePink
+//  Git         :   https://github.com/SoftIcePink/cfpt_m152_portefolio
+//  File        :   dbconnexion.php
+//  Description :   This file is used as the model for the connexion with the database
+//                  The user has restricted rights.
+//  Last update :   19.02.2019
+
+function connectDB(){
+	$dbServer = "127.0.0.1";
+	$dbName = "fb";
+	$dbUser = "m152";
+	$dbPwd = "1234";
+	
+	static $bdd = null;
+	
+	if ($bdd === null) {
+		$bdd = new PDO("mysql:host=$dbServer;dbname=$dbName;charset=utf8", $dbUser, $dbPwd);
+		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	}
+	return $bdd;
+	
+}
---
+++ models/posts.php
@@ -0,0 +1,18 @@
+<?php
+
+//  Projet      :   Portefolio - M152
+//  Author      :   SoftIcePink
+//  Git         :   https://github.com/SoftIcePink/cfpt_m152_portefolio
+//  File        :   posts.php
+//  Description :   This file is used as the model of posts
+//  Last update :   19.02.2019
+
+require_once 'models/dbconnexion.php';
+
+/**
+ * gets all posts from the posts table as well as every media that were attached to each posts
+ * @return array table contains all the records from the table
+ */
+function getAllPosts(){
+
+}
