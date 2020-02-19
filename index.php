ea6e283f35db1d535684274aa6534510f75d9a6b
SoftIce <32594805+SoftIcePink@users.noreply.github.com>
2020-02-19 22:05:45 +0100

    New router for the MVC frame

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+//  Projet      :   Portefolio - M152
+//  Author      :   SoftIcePink
+//  Git         :   https://github.com/SoftIcePink/cfpt_m152_portefolio
+//  File        :   index.php
+//  Description :   This file is used as the router for this app.
+//  Last update :   19.02.2019
+
+$action = filter_input(INPUT_GET, "action", FILTER_SANITIZE_URL);
+if (empty($action)) {
+    $action = 'index';
+}
+
+
+try {
+    switch ($action) {
+        case 'index' :
+            require_once 'controllers/index.php';
+            break;
+
+        case 'addpost':
+            require_once 'controllers/addpost.php';
+        break;
+        
+        default:
+            require_once 'views/404.php';
+        break;
+    }
+} catch (Exception $e) {
+    require_once 'views/500.php';
+
+}
+
