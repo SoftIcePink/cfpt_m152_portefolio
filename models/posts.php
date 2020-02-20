e3ea791bfd60787ce69661ed8a97423dab595ea6
SoftIce <32594805+SoftIcePink@users.noreply.github.com>
2020-02-20 11:34:47 +0100

    Added functions
    To do : Define, bindparams, etc.

--- models/posts.php
+++ models/posts.php
@@ -14,5 +14,53 @@ require_once 'models/dbconnexion.php';
  * @return array table contains all the records from the table
  */
 function getAllPosts(){
+    $db = connectDB();
+    $sql = "SELECT posts.idPost, posts.commentaire, posts.creationDate, posts.modificationDate, medias.nomMedia, medias.idMedia, medias.typeMedia
+            FROM posts 
+            JOIN medias ON posts.idPost=medias.idPost;";
+    $request = $db->prepare($sql);
+    $request->execute();
+    return $request->fetchAll(PDO::FETCH_ASSOC);
+}
+
+/**
+ * Adds a post which contains no images.
+ */
+function addPost($commentaire, $creationDatePost, $modificationDatePost){
+    $db = connectDB();
+    $sql = "INSERT INTO posts("
+}
+
+/**
+ * Adds a post which contains one or multiple images.
+ */
+function addPost($commentaire, $creationDatePost, $modificationDatePost, $media){
+    foreach($media as $m){
+
+    }
+}
+
+
+function addMedia($mediaType, $mediaName, $idPost){
+    $db = connectDB();
+    $sql = "INSERT INTO medias(typeMedia,nomMedia, creationDate, modificationDate,  "
+}
+
+/**
+ * Edits a post which contains no images.
+ */
+function editPost($idPost, $commentaire, )
+
+/**
+ * Edits a post which contains 1 or multiple images.
+ */
+function editPost($idPost, $commentaire,){
+    
+}
+
+/**
+ * Deletes a post.
+ */
+function deletePost(){
 
 }
